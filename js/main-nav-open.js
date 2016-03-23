1b73f2950ebbf50f4b3550796d50e373b4fbe30d
JuliaAf <a290288@gmail.com>
2016-03-23 19:39:56 +0300

    flexbox all pages

--- js/main-nav-open.js
+++ js/main-nav-open.js
@@ -5,10 +5,5 @@ var popup = document.querySelector(".main-nav");
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("main-nav--hide");
-  // link.classList.toggle("main-nav-btn--close");
-});
-
-link.addEventListener("click", function(event) {
-  event.preventDefault();
   link.classList.toggle("main-nav-btn--close");
 });
